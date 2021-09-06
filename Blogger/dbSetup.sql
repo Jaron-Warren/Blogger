4ecb4cf6766f5f4b9d79f2f994fd0334879ec93a
Jaron-Warren <Jarwarren@gmail.com>
2021-09-05 22:44:39 -0600

    DB setup and transients

--- Blogger/dbSetup.sql
+++ Blogger/dbSetup.sql
@@ -6,3 +6,21 @@ CREATE TABLE IF NOT EXISTS accounts(
   email varchar(255) COMMENT 'User Email',
   picture varchar(255) COMMENT 'User Picture'
 ) default charset utf8 COMMENT '';
+CREATE TABLE IF NOT EXISTS blogs(
+  id INT NOT NULL primary key COMMENT 'primary key',
+  createdAt DATETIME DEFAULT CURRENT_TIMESTAMP COMMENT 'Time Created',
+  updatedAt DATETIME DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP COMMENT 'Last Update',
+  title varchar(20) COMMENT 'Blog Title',
+  body varchar(255) COMMENT 'Blog body',
+  imgUrl varchar(255) COMMENT 'Blog picture',
+  published TINYINT COMMENT 'Has Blog been published?',
+  creatorId varchar(255) COMMENT 'Creator Id'
+) default charset utf8 COMMENT '';
+CREATE TABLE IF NOT EXISTS comments(
+  id INT NOT NULL primary key COMMENT 'primary key',
+  createdAt DATETIME DEFAULT CURRENT_TIMESTAMP COMMENT 'Time Created',
+  updatedAt DATETIME DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP COMMENT 'Last Update',
+  creatorId varchar(255) COMMENT 'Creator Id',
+  body varchar(240) NOT NULL COMMENT 'comment body',
+  blog varchar(255) NOT NULL COMMENT 'blog comment belongs to'
+) default charset utf8 COMMENT '';
